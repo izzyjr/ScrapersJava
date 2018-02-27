36e028c3984539a506f8317c239e70cbf806d1e2
Israel Mesa <mesa.israel50@gmail.com>
2018-02-26 22:46:54 -0500

    google web scraper - java

--- googleApple/src/main/java/GoogleScraper.java
+++ googleApple/src/main/java/GoogleScraper.java
@@ -1,5 +1,7 @@
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 
+import java.io.PrintWriter;
 import java.net.URLEncoder;
 
 public class GoogleScraper {
@@ -13,6 +15,19 @@ public class GoogleScraper {
 
         final org.jsoup.nodes.Document page = Jsoup.connect("https://www.google.com/search?q=" + URLEncoder.encode(query,"UTF-8")).get();
 
-        System.out.println(page.outerHtml());
+//        System.out.println(page.outerHtml());
+
+        final PrintWriter out = new PrintWriter("results.txt");
+
+
+        for (Element searchResult: page.select("H3.r a")) {
+
+            final String title = searchResult.text();
+            final String url = searchResult.attr("href");
+
+            out.write(title + " -> " + url + "\n");
+        }
+
+        out.close();
     }
 }
