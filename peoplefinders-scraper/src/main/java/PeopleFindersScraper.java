514229ca164a8c511b4e96a4e37cd04d5e770625
Israel Mesa <mesa.israel50@gmail.com>
2018-03-05 19:21:24 -0500

    PeopleFindersScraper

--- peoplefinders-scraper/src/main/java/PeopleFindersScraper.java
+++ peoplefinders-scraper/src/main/java/PeopleFindersScraper.java
@@ -1,12 +1,29 @@
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class PeopleFindersScraper {
 
     public static void main(String[] args) throws Exception {
 
-        final HttpResponse<String> response = Unirest.get("https://www.peoplefinders.com/Widget/GetWidgets?ln=smith&city=New%20York&state=NY&resultType=multiple").asString();
+        final String lastName = "smith";
+        final String city = "New York";
+        final String state = "New York";
 
-        System.out.println(response.getBody());
+        final HttpResponse<String> response = Unirest.get("https://www.peoplefinders.com/Widget/GetWidgets?&resultType=multiple")
+                .queryString("ln", lastName)
+                .queryString("LastName", lastName)
+                .queryString("city", city)
+                .queryString("state", state)
+                .asString();
+
+        final Document htmlSnippet = Jsoup.parseBodyFragment(response.getBody());
+
+        for (Element peopleResult : htmlSnippet.select("div.row")){
+            System.out.println(peopleResult.child(0).text());
+            System.out.println("..........................");
+        }
     }
 }
