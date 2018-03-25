e0783a877daad79e5314f73de30aac353955b51b
Israel Mesa <mesa.israel50@gmail.com>
2018-03-25 19:35:07 -0400

    AS daily news Scraper

--- fifaNewsScraper/src/main/java/fifaScraper.java
+++ fifaNewsScraper/src/main/java/fifaScraper.java
@@ -19,22 +19,23 @@ public class fifaScraper {
 
         WebElement element = driver.findElement(By.xpath("//*[@id=\"buscador-menu\"]"));
 
-        element.sendKeys("Messi");
+        element.sendKeys("Lionel Messi");
 
         element.submit();
 
         driver.findElement(By.xpath("/html/body/div[6]/div[2]/div[1]/div[2]/ul/li[3]/button")).click();
 
-        System.out.println("Page title is: " + driver.getTitle());
+        System.out.println(driver.getTitle());
 
-        List<WebElement> link = driver.findElements(By.xpath("/html/body/div[6]/div[2]/div[1]/div[3]/section/article[1]/div[1]/h1/a"));
-        List<WebElement> summary = driver.findElements(By.xpath("/html/body/div[6]/div[2]/div[1]/div[3]/section/article[1]/div[1]/p"));
-        List<WebElement> date = driver.findElements(By.xpath("/html/body/div[6]/div[2]/div[1]/div[3]/section/article[1]/div[1]/div/div[1]/p/time/a/span[1]"));
+        List<WebElement> link = driver.findElements(By.className("titular-articulo"));
+        List<WebElement> summary = driver.findElements(By.className("txt"));
+        List<WebElement> date = driver.findElements(By.className("fecha"));
 
-        for (int j = 0; j < link.size(); j++) {
-            System.out.println( link.get(j).getText() + " - " +
-                    "\n" + summary.get(j).getText() +
-                    "\n" + date.get(j).getText()) ;
+        for (int i = 0; i < link.size(); i++) {
+            System.out.println("============= News Article =========================");
+            System.out.println(date.get(i).getText() +
+                    "\n" + link.get(i).getText() + ": " +
+                    "\n" + summary.get(i).getText());
         }
 
         driver.quit();
@@ -42,39 +43,4 @@ public class fifaScraper {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    WebElement elementSearch = driver.findElement(By.xpath("/html/body/header/div[3]/div/div[4]/div/div/div/form/input"));
-//
-//        elementSearch.sendKeys("Lionel Messi");
-//
-//                elementSearch.submit();
-//
-//                System.out.println("Page title is: " + driver.getTitle());
-//
-//                List<WebElement> link = driver.findElements(By.xpath("//*[@id=\"search-all\"]/section/div[2]/div/div/div/div/div/div[2]/div/a/h3"));
-//        List<WebElement> summary = driver.findElements(By.xpath("//*[@id=\"search-all\"]/section/div[2]/div/div/div/div/div/div[1]/div/p"));
-//
-//        System.out.println(" =============== Fifa Articles by Player ================= ");
-//        for (int j = 0; j < link.size(); j++) {
-//        System.out.println( link.get(j).getText() + ": " +
-//        '\n' + summary.get(j).getText() ) ;
-//        }
 
