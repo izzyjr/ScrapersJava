d8b061954703ba7901a1dbc5476b2c221c830370
Israel Mesa <mesa.israel50@gmail.com>
2018-04-12 17:05:41 -0400

    Coin Symbols

--- yahooFinanceScraper/src/main/java/Driver.java
+++ yahooFinanceScraper/src/main/java/Driver.java
@@ -1,5 +1,9 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class Driver {
 
@@ -12,6 +16,12 @@ public class Driver {
         driver.get("https://finance.yahoo.com/cryptocurrencies?count=150");
 
         System.out.println(driver.getTitle());
+
+        List<WebElement> symbol = driver.findElements(By.xpath("//*[@id=\"scr-res-table\"]/table/tbody/tr[*]/td[2]/a"));
+
+        for (int i = 0; i < symbol.size(); i++) {
+            System.out.println(symbol.get(i).getText());
+        }
 
         driver.quit();
     }
